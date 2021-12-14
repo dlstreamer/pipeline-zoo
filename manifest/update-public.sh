0cc92d31b4507f21d90ce62b0f5ec17668b65ecd
nnshah1 <neelay.n.shah@intel.com>
2021-12-14 02:56:18 -0800

    convenience script for updating public-init branch

---
+++ manifest/update-public.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+#
+# Copyright (C) 2019-2021 Intel Corporation.
+#
+# SPDX-License-Identifier: MIT
+#
+
+MANIFEST_DIR=$(dirname "$(readlink -f "$0")")
+SOURCE_DIR=$(dirname $MANIFEST_DIR)
+
+cp $SOURCE_DIR/manifest/public.txt $SOURCE_DIR/.public.txt
+cp $SOURCE_DIR/manifest/internal.txt $SOURCE_DIR/.internal.txt
+
+git checkout origin/main
+git branch -D public-updates || true
+git checkout -b public-updates
+
+git filter-repo --refs public-updates --paths-from-file public.txt --force
+git filter-repo --refs public-updates --paths-from-file internal.txt --force --invert
+
+git checkout public-init
+
+git merge public-updates --squash --allow -X theirs
+
+git commit -m "updates to public"
+
+git push origin public-init:public-init
+
+git branch -D public-updates
+
+git checkout main
