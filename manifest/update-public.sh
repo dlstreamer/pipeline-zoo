9acbe88ed7d357977dae3d73f43dd0d60374cdf0
Neelay Shah <neelay.n.shah@intel.com>
2022-12-07 17:57:17 -0800

    updates for yolov5 in public.txt

--- manifest/update-public.sh
+++ manifest/update-public.sh
@@ -7,44 +7,54 @@
 
 MANIFEST_DIR=$(dirname "$(readlink -f "$0")")
 SOURCE_DIR=$(dirname $MANIFEST_DIR)
+BRANCH=main
 
 cp $SOURCE_DIR/manifest/public.txt $SOURCE_DIR/.public.txt
 cp $SOURCE_DIR/manifest/internal.txt $SOURCE_DIR/.internal.txt
 rm -rf .staging
 
+# Create staging area
+
 git clone https://github.com/intel-innersource/frameworks.ai.media-analytics.pipeline-zoo.git .staging
 
 cd .staging
 
-git checkout origin/main
+git checkout origin/$BRANCH
 git branch -D public-updates || true
 git checkout -b public-updates
 
+# Explicitly keep files and directories listed in public.txt (rewrite history for public-updates)
 git filter-repo --refs public-updates --paths-from-file $SOURCE_DIR/.public.txt --force
+
+# Explicitly remove files and directories listed in internal.txt (rewrite history for public-updates)
 git filter-repo --refs public-updates --paths-from-file $SOURCE_DIR/.internal.txt --force --invert
 
-#git add --all
+# Add public remote
+git remote rm public-origin || true
+git remote add public-origin https://github.com/dlstreamer/pipeline-zoo.git
+git fetch public-origin
 
-#git commit -m "adding new files"
+# Reset public-main to main of public-origin
+# Push to origin
+git checkout -b public-main
+git reset --hard public-origin/main
+git push --force origin public-main
 
-git checkout public-main
-git reset --hard origin/public-main
-
+# Merge public-updates ontop of public-main
 git merge public-updates --squash --allow -X theirs
 git rm -rf .
 git checkout public-updates -- .
 git commit --no-edit
-
 
 MESSAGE=$1
 if [ -z "$1" ]; then
     MESSAGE="updates to public"
 fi
 
-#git commit -m "$MESSAGE"
-#git commit --no-edit
+# Force updates to public-staging
 
 git push origin public-main:public-staging --force
-#git branch -D public-updates
 
-# git checkout main
+# After verified can do
+# git push public-origin public-staging:public-staging
+# merge from public github
